aa2535b1e5f652f952ab4928e68116551755c5f3
MingsonZheng <MingsonZheng@outlook.com>
2021-12-17 11:55:46 +0800

    项目演示

---
+++ src/FrontEnd/Controllers/DaprController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DaprController : ControllerBase
+    {
+        public DaprController()
+        {
+        }
+
+        // 通过HttpClient调用BackEnd
+        [HttpGet]
+        public async Task<ActionResult> GetAsync()
+        {
+            using var httpClient = DaprClient.CreateInvokeHttpClient();
+            var result = await httpClient.GetAsync("http://backend/WeatherForecast");
+            var resultContent = $"result is {result.StatusCode} {await result.Content.ReadAsStringAsync()}";
+            return Ok(resultContent);
+        }
+
+        // 通过DaprClient调用BackEnd
+        [HttpGet("get2")]
+        public async Task<ActionResult> Get2Async()
+        {
+            using var daprClient = new DaprClientBuilder().Build();
+            var result = await daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(HttpMethod.Get, "backend", "WeatherForecast");
+            return Ok(result);
+        }
+    }
+}
---
+++ src/FrontEnd/Controllers/DaprDIController.cs
@@ -0,0 +1,27 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DaprDIController : ControllerBase
+    {
+        private readonly DaprClient _daprClient;
+
+        public DaprDIController(DaprClient daprClient)
+        {
+            _daprClient = daprClient;
+        }
+
+        [HttpGet()]
+        public async Task<ActionResult> GetAsync()
+        {
+            var result = await _daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(HttpMethod.Get, "backend", "WeatherForecast");
+            return Ok(result);
+        }
+    }
+}
